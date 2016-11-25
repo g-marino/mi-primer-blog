08dc6bfd18f15243aed42191cbc59bb2ed5b456e
Gime Marino <Gimenamarino85@gmail.com>
2016-11-25 16:52:35 -0200

    Mi app Django Girls, primer commit

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+def hi(name):
+    if name == "gime":
+         print("hi gime")
+    elif name == "LEANDRO" :
+         print ("hola leandrico")
+    else:
+         print("Hi anonymous!")
+
+hi(input("cual es su nombre?"))
